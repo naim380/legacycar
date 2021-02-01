7b840b14e62a47c586054b3bea6c717da7a31335
naim380 <75119537+naim380@users.noreply.github.com>
2021-02-01 10:42:10 +0100

    premiere partie etape 2

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,1 @@
+<h1>HelloWorld From Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</h1>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
-Route::get('/', function () {
-    return view('test');
+Route::get('/product', function () {
+    return view('products-list');
+});
+Route::get('/product/{id}', function () {
+    return view('product-details');
+});
+Route::get('/cart', function () {
+    return view('cart');
 });
