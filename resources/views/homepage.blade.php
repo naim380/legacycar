b077c9ca6195f3c1974dd2f3f30e9c30ef299d7f
naim380 <naim.ebrahim@le-campus-numerique.fr>
2021-02-02 19:49:03 +0100

    3.1

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,1 +1,13 @@
-<h1>home page</h1>
+@extends('layout')
+
+@section('content')
+
+    <h1>home page</h1>
+@endsection
+
+@extends('layout')
+
+@section('content')
+
+    <h1>home page</h1>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+    <title> Naïm EBRAHIM CV Expérience formation</title>
+    <meta name="keyword" CV Expérience formation>
+    <meta name="description" Mon CV avec expérience et formations>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+@yield('content')
+<footer>
+        Naïm EBRAHIM
+        28 bis rue de la résistance
+        naimebrahim38@gmail.com
+</footer>
+
+</body>
+</html>
